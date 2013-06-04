f77a626f2bf3963c117e38386f4c807faca8d6de
Till Theis <theis.till@gmail.com>
2013-06-04 18:21:26 +0200

    enumerators erstellen

---
+++ code/examples/src/reactive/streams/Enumerators.scala
@@ -0,0 +1,79 @@
+package examples.reactive.streams
+
+import play.api.libs.iteratee._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+object Enumerators {
+  val sumIteratee: Iteratee[Int, Int] = Iteratee.fold(0)(_ + _)
+
+  val e1: Enumerator[Int] = Enumerator(1, 2, 3)
+  val e2: Enumerator[Int] = Enumerator(4, 5)
+
+  object Creation {
+    case class NumberEnumerator(xs: Int*) extends Enumerator[Int] {
+      def apply[A](iteratee: Iteratee[Int, A]):
+          Future[Iteratee[Int, A]] = {
+        xs.foldLeft(Future(iteratee)) { (futureIteratee, x) =>
+          futureIteratee.flatMap { iteratee =>
+            iteratee.fold(_ match {
+              case Step.Cont(k) => Future(k(Input.El(x)))
+              case _ => Future(iteratee)
+            })
+          }
+        }
+      }
+    }
+
+    val numberEnumeratorFromInheritance: Enumerator[Int] =
+      NumberEnumerator(1, 4, -2)
+
+    val numberEnumeratorFromApply: Enumerator[Int] =
+      Enumerator(1, 4, -2)
+
+    def numberEnumerator(xs: Int*): Enumerator[Int] = {
+      Concurrent.unicast { channel =>
+        xs.foreach(channel.push(_))
+        channel.end
+      }
+    }
+
+    val numberEnumeratorFromUnicast: Enumerator[Int] =
+      numberEnumerator(1, 4, -2)
+  }
+
+  object NestedApplication {
+    val sumIterateeAfterE1: Future[Iteratee[Int, Int]] =
+      e1(sumIteratee)
+
+    val sumIterateeAfterE2: Future[Future[Iteratee[Int, Int]]] =
+      sumIterateeAfterE1.map(e2(_))
+
+    val sumResult: Future[Future[Future[Int]]] =
+      sumIterateeAfterE2.map(_.map(_.run))
+  }
+
+  object FlattenedApplication {
+    val sumIterateeAfterE1: Iteratee[Int, Int] =
+      Iteratee.flatten(e1(sumIteratee))
+
+    val sumIterateeAfterE2: Iteratee[Int, Int] =
+      Iteratee.flatten(e2(sumIterateeAfterE1))
+
+    val sumResult: Future[Int] = sumIterateeAfterE2.run
+  }
+
+  object CombinedApplication {
+    val e1e2: Enumerator[Int] = e1.andThen(e2)
+
+    val sumIterateeAfterE1E2: Iteratee[Int, Int] =
+      Iteratee.flatten(e1e2(sumIteratee))
+
+    val sumResult: Future[Int] = sumIterateeAfterE1E2.run
+  }
+
+  object ApplicationWithRun {
+    val e1e2: Enumerator[Int] = e1.andThen(e2)
+    val sumResult: Future[Int] = e1e2.run(sumIteratee)
+  }
+}
---
+++ code/examples/test/reactive/streams/EnumeratorsSpec.scala
@@ -0,0 +1,93 @@
+import org.specs2.mutable._
+import org.specs2.time.NoTimeConversions
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import play.api.libs.iteratee._
+import play.api.libs.iteratee.Enumerator.enumInput
+
+import examples.reactive.streams.Enumerators
+
+class EnumeratorsSpec extends Specification with NoTimeConversions {
+  Seq(
+    ("inheritance", Enumerators.Creation.numberEnumeratorFromInheritance),
+    ("apply",       Enumerators.Creation.numberEnumeratorFromApply),
+    ("unicast",     Enumerators.Creation.numberEnumeratorFromUnicast)
+  ).foreach { pair =>
+    val (kind, enumerator) = pair
+
+    kind + " enumerators" should {
+      "enumerate correctly" in {
+        val iteratee = Enumerators.sumIteratee
+        val sum = Await.result(enumerator.run(iteratee), 1 second)
+        sum === 3
+      }
+    }
+  }
+
+  "Enumerators with nested application" should {
+    import Enumerators.NestedApplication._
+
+    "enumerate the initial iteratee correctly" in {
+      val iteratee = Await.result(sumIterateeAfterE1, 1 second)
+      val sum = Await.result(iteratee.run, 1 second)
+      sum === 6
+    }
+
+    "enumerate the resulting iteratee correctly" in {
+      val futureIteratee = Await.result(sumIterateeAfterE2, 1 second)
+      val iteratee = Await.result(futureIteratee, 1 second)
+      val sum = Await.result(iteratee.run, 1 second)
+      sum === 15
+    }
+
+    "lead to a correct result" in {
+      val d = 1 second
+      val sum: Int = Await.result(Await.result(Await.result(sumResult, d), d), d)
+      sum === 15
+    }
+  }
+
+  "Enumerators with flattened application" should {
+    import Enumerators.FlattenedApplication._
+
+    "enumerate the initial iteratee correctly" in {
+      val sum = Await.result(sumIterateeAfterE1.run, 1 second)
+      sum === 6
+    }
+
+    "enumerate the resulting iteratee correctly" in {
+      val sum = Await.result(sumIterateeAfterE2.run, 1 second)
+      sum === 15
+    }
+
+    "lead to a correct result" in {
+      val sum: Int = Await.result(sumResult, 1 second)
+      sum === 15
+    }
+  }
+
+  "Enumerators with combined application" should {
+    import Enumerators.CombinedApplication._
+
+    "enumerate the initial iteratee correctly" in {
+      val sum = Await.result(sumIterateeAfterE1E2.run, 1 second)
+      sum === 15
+    }
+
+    "lead to a correct result" in {
+      val sum: Int = Await.result(sumResult, 1 second)
+      sum === 15
+    }
+  }
+
+  "Enumerators with combined application" should {
+    import Enumerators.ApplicationWithRun._
+
+    "lead to a correct result" in {
+      val sum: Int = Await.result(sumResult, 1 second)
+      sum === 15
+    }
+  }
+}
