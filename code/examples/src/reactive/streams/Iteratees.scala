5249d7c1b1547be2b3efe749a691fefc0cd71196
Till Theis <theis.till@gmail.com>
2013-07-03 15:20:42 +0200

    explicit match => partial function

--- code/examples/src/reactive/streams/Iteratees.scala
+++ code/examples/src/reactive/streams/Iteratees.scala
@@ -9,7 +9,7 @@ object Iteratees extends App {
 
     case class SumIteratee(sum: Int = 0) extends Iteratee[Int, Int] {
       def fold[B](folder: Step[Int, Int] => Future[B]): Future[B] = {
-        folder(Step.Cont(_ match {
+        folder(Step.Cont {
           case Input.El(i) => SumIteratee(sum + i)
           case Input.Empty => this
           case Input.EOF   => new Iteratee[Int, Int] {
@@ -17,18 +17,18 @@ object Iteratees extends App {
               folder(Step.Done(sum, Input.EOF))
             }
           }
-        }))
+        })
       }
     }
 
     val sumIterateeFromInheritance: Iteratee[Int, Int] = SumIteratee()
 
 
-    def sumIteratee(sum: Int = 0): Iteratee[Int, Int] = Cont(_ match {
+    def sumIteratee(sum: Int = 0): Iteratee[Int, Int] = Cont {
       case Input.El(i) => sumIteratee(sum + i)
       case Input.Empty => sumIteratee(sum)
       case Input.EOF   => Done(sum, Input.EOF)
-    })
+    }
 
     val sumIterateeFromConstructor: Iteratee[Int, Int] = sumIteratee()
 
@@ -41,10 +41,10 @@ object Iteratees extends App {
       def folder_(xs: List[Int])(step: Step[Int, Int]): Future[Int] =
         xs match {
           case Nil => step match {
-            case Step.Cont(k) => k(Input.EOF).fold(_ match {
+            case Step.Cont(k) => k(Input.EOF).fold {
               case Step.Done(sum, Input.EOF) => Future(sum)
               case _ => Future.failed(new Exception("invalid state"))
-            })
+            }
             case _ => Future.failed(new Exception("invalid state"))
           }
           case x :: xs => step match {
