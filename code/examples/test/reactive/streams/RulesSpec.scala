3faf306c670e0922686f2475d5a57c5dbd2b29f1
Till Theis <theis.till@gmail.com>
2013-08-19 18:12:18 +0200

    kiselyovs regeln auch mit text

--- code/examples/test/reactive/streams/RulesSpec.scala
+++ code/examples/test/reactive/streams/RulesSpec.scala
@@ -142,13 +142,12 @@ class RulesSpec extends Specification with NoTimeConversions {
 
   // EFFECTFUL ITERATEES
   {
-    type E = Char
-    type I[A] = Iteratee[E, A]
-    type M[A] = Future[A]
-    type IM[A] = M[I[A]]
+    type E     = Char           // Der Eingabetyp der Iteratees (Char)
+    type I[A]  = Iteratee[E, A] // Ein Iteratee von Char nach A
+    type M[A]  = Future[A]      // Die Monade ist immer Future
+    type IM[A] = M[I[A]]        // Ein Iteratee in der Future-Monade
 
-    def en_str[A](s: String): I[A] => IM[A] =
-      Enumerator.enumerate(s).apply(_)
+    def en_str(s: String): Enumerator[E] = Enumerator.enumerate(s)
 
     def runM[A](mi: IM[A]): M[A] = Iteratee.flatten(mi).run
 
@@ -167,20 +166,16 @@ class RulesSpec extends Specification with NoTimeConversions {
 
     "the composition of effectful enumerators" should {
       "correspond to the concatenation of their sources." in {
-        implicit class KleisliOperator[A, B](f1: A => M[B]) {
-          def >>>[C](f2: B => M[C]): A => M[C] = x => f1(x).flatMap(f2)
-        }
-
         val s1: String = "foo"
         val s2: String = "bar"
 
-        val left: I[String] => IM[String] = en_str(s1 + s2)
-        val right: I[String] => IM[String] = en_str[String](s1) >>> en_str[String](s2)
+        val left: Enumerator[E] = en_str(s1 + s2)
+        val right: Enumerator[E] = en_str(s1).andThen(en_str(s2))
 
         val i: I[String] = prefixIteratee("foo")
 
-        runM(left(i)).foreach(println)
-        runM(right(i)).foreach(println)
+        // runM(left(i)).foreach(println) // with this enabled there will be a wrong result
+        // runM(right(i)).foreach(println) // with this enabled there will be a wrong result
 
         await(runM(left(i))) === await(runM(right(i)))
       }
@@ -189,7 +184,7 @@ class RulesSpec extends Specification with NoTimeConversions {
 
     "an effectful iteratee that recognizes the string s" should {
       "recognize (s+s2) for any s2" in {
-        def bind[A, B](m: IM[A], f: A => IM[B]): IM[B] =
+        def flatMap[A, B](m: IM[A], f: A => IM[B]): IM[B] =
           m.map(_.flatMapM(f))
 
         val s1: String = "foo"
@@ -199,10 +194,10 @@ class RulesSpec extends Specification with NoTimeConversions {
         val f: String => I[String] = x => Done(s"f($x)")
 
         val left: IM[String] = en_str(s1 + s2)(i.flatMap(f))
-        val right: IM[String] = bind(en_str(s1)(i), en_str(s2).compose(f))
+        val right: IM[String] = flatMap(en_str(s1)(i), (x: String) => en_str(s2)(f(x)))
 
-        runM(left).foreach(println)
-        runM(right).foreach(println)
+        // runM(left).foreach(println)
+        // runM(right).foreach(println)
 
         await(runM(left)) === await(runM(right))
       }
@@ -219,8 +214,8 @@ class RulesSpec extends Specification with NoTimeConversions {
         val left: IM[String] = en_str(s)(i.flatMap(f))
         val right: IM[String] = en_str(s)(i).map(_.flatMap(f))
 
-        runM(left).foreach(println)
-        runM(right).foreach(println)
+        // runM(left).foreach(println)
+        // runM(right).foreach(println)
 
         def matchNotRecognizingIteratee(i: IM[String]) =
           await(runM(i)) must throwA[RuntimeException].like {
@@ -260,25 +255,34 @@ class RulesSpec extends Specification with NoTimeConversions {
 
 
     "flatMap over effectful iteratees" should {
-      "be right distributive" in {
-        implicit class LeftBiasedAlternativeOperator[A](i1: I[A]) {
-          def <|(i2: I[A]): I[A] = {
-            def toFuturePair[A](p: (Future[A], Future[A])): Future[(A, A)] =
-              Future.sequence(Seq(p._1, p._2)) map { case Seq(x, y) => (x, y) }
+      "be right distributive if it is idempotent" in {
+        def toFuturePair[A](p: (Future[A], Future[A])): Future[(A, A)] =
+          Future.sequence(Seq(p._1, p._2)).map { case Seq(x, y) => (x, y) }
 
-            def flatFeed(i: I[A], in: Input[E]): I[A] = Iteratee.flatten(i.feed(in))
+        def isIdempotent[A](i: I[A]) = {
+          val s: String = "foobar"
+          val left: M[(I[A], I[A])] = en_str(s)(i).flatMap(x => Future((x, x)))
+          val right: M[(I[A], I[A])] = en_str(s)(i).flatMap(x => en_str(s)(i).flatMap(y => Future((x, y))))
+          val leftResult: M[(A, A)] = left.flatMap(p => toFuturePair(p._1.run, p._2.run))
+          val rightResult: M[(A, A)] = right.flatMap(p => toFuturePair(p._1.run, p._2.run))
+//          println("left: " + await(leftResult) + " right: " + await(rightResult))
+          await(leftResult) == await(rightResult)
+        }
 
-            val doneM = toFuturePair(Iteratee.isDoneOrError(i1), Iteratee.isDoneOrError(i2))
-            Iteratee.flatten(doneM.map {
-              case (true, _) => i1
-              case (_, true) => i2
-              case _         => new Iteratee[E, A] {
-                def fold[B](folder: (Step[E, A]) => Future[B]): Future[B] = folder(Step.Cont {
-                  in => flatFeed(i1, in) <| flatFeed(i2, in)
-                })
-              }
-            })
-          }
+        // left biased alternative
+        def alternative[A](i1: I[A], i2: I[A]): I[A] = {
+          def flatFeed(i: I[A], in: Input[E]): I[A] = Iteratee.flatten(i.feed(in))
+
+          val doneM = toFuturePair(Iteratee.isDoneOrError(i1), Iteratee.isDoneOrError(i2))
+          Iteratee.flatten(doneM.map {
+            case (true, _) => i1
+            case (_, true) => i2
+            case _         => new Iteratee[E, A] {
+              def fold[B](folder: (Step[E, A]) => Future[B]): Future[B] = folder(Step.Cont {
+                in => alternative(flatFeed(i1, in), flatFeed(i2, in))
+              })
+            }
+          })
         }
 
         val i: I[String] = Iteratee.head.map(_ => "i")
@@ -286,15 +290,18 @@ class RulesSpec extends Specification with NoTimeConversions {
         val k1: String => I[String] = x => Iteratee.head.map(x => s"k1($x)")
         val k2: String => I[String] = x => Done(s"k2($x)")
 
-        val left: I[String] = i.flatMap(x => k1(x) <| k2(x))
-        val right: I[String] = i.flatMap(k1) <| i.flatMap(k2)
+        val left: I[String] = i.flatMap(x => alternative(k1(x), k2(x)))
+        val right: I[String] = alternative(i.flatMap(k1), i.flatMap(k2))
 
         def run[A](i: I[A]) = runM(en_str("foobar")(i))
 
-        run(left).foreach(println)
-        run(right).foreach(println)
+//        run(left).foreach(println)
+//        run(right).foreach(println)
 
-        await(run(left)) === await(run(right))
+        val matchIdempotence = isIdempotent(i) === true
+        val matchEquality = await(run(left)) === await(run(right))
+
+        matchIdempotence and matchEquality
       }
     }
   }
