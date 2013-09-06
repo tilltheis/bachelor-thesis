3efe7b6111ff7ccef8a3c3235027d9246cc6d6b3
Till Theis <theis.till@gmail.com>
2013-09-06 21:06:11 +0200

    monadische gesetze fuer einige typen der stdlib

--- code/examples/src/reactive/monads/KleisliTriple.scala
+++ code/examples/src/reactive/monads/KleisliTriple.scala
@@ -2,6 +2,8 @@ package examples.reactive.monads
 
 import scala.language.higherKinds
 
+import scala.util.Try
+
 trait KleisliTriple[M[_]] {
   def unit[A](a: A): M[A]
   def map[A, B](f: A => B, m: M[A]): M[B]
@@ -13,5 +15,17 @@ object KleisliTriple {
     def unit[A](a: A): Option[A] = Some(a)
     def map[A, B](f: A => B, m: Option[A]): Option[B] = m.map(f)
     def bind[A, B](f: A => Option[B]): Option[A] => Option[B] = m => m.flatMap(f)
+  }
+
+  implicit object ListKleisliTriple extends KleisliTriple[List] {
+    def unit[A](a: A): List[A] = List(a)
+    def map[A, B](f: A => B, m: List[A]): List[B] = m.map(f)
+    def bind[A, B](f: A => List[B]): List[A] => List[B] = m => m.flatMap(f)
+  }
+
+  implicit object TryKleisliTriple extends KleisliTriple[Try] {
+    def unit[A](a: A): Try[A] = Try(a)
+    def map[A, B](f: A => B, m: Try[A]): Try[B] = m.map(f)
+    def bind[A, B](f: A => Try[B]): Try[A] => Try[B] = m => m.flatMap(f)
   }
 }
--- code/examples/test/reactive/monads/LawSpec.scala
+++ code/examples/test/reactive/monads/LawSpec.scala
@@ -1,42 +1,54 @@
 package examples.reactive.monads
 
+import scala.language.higherKinds
+
+import scala.util.Try
+
 import org.specs2.mutable._
 
 class LawSpec extends Specification {
+
+  implicit class Monad[M[_], A](m: M[A])(implicit kleisli: KleisliTriple[M]) {
+    def map[B](f: A => B): M[B] = kleisli.map(f, m)
+    def flatMap[B](f: A => M[B]): M[B] = kleisli.bind(f)(m)
+  }
 
   type T = Char
   type U = Int
   type V = String
 
-  def f(x: T): M[U] = unit(x.toInt)
-  def g(x: U): M[V] = unit(x.toString)
+  testMonad(Option('a'), "Option (Some)")
+  testMonad(Option.empty[T], "Option (None)")
+  testMonad(List('a', 'b', 'c'), "List")
+  testMonad(List.empty[T], "List (empty)")
+  testMonad(Try('a'), "Try (Success)")
+  testMonad(Try[T](throw new RuntimeException), "Try (Failure)")
 
-  val x: T = 'a'
+  def testMonad[M[_]](m: M[T], description: String)(implicit kleisli: KleisliTriple[M]) = {
+    def unit[A](x: A): M[A] = kleisli.unit(x)
 
-  type M[A] = Option[A]
-  def unit[A](x: A): M[A] = Some(x)
-  val m: M[T] = Some('a')
+    def f(x: T): M[U] = unit(x.toInt)
+    def g(x: U): M[V] = unit(x.toString)
 
-  // type M[A] = List[A]
-  // def unit[A](x: A): M[A] = List(x)
-  // val m: M[T] = List('a', 'b', 'c')
+    val x: T = 'a'
 
-
-  "left neutral element law" should {
-    "hold" in {
-      unit(x).flatMap(f) === f(x)
+    "left neutral element law" should {
+      s"hold for $description" in {
+        unit(x).flatMap(f) === f(x)
+      }
     }
-  }
 
-  "right neutral element law" should {
-    "hold" in {
-      m.flatMap(unit) === m
+    "right neutral element law" should {
+      s"hold for $description" in {
+        m.flatMap(unit) === m
+      }
     }
-  }
 
-  "associativity law" should {
-    "hold" in {
-      m.flatMap(f).flatMap(g) === m.flatMap(x => f(x).flatMap(g))
+    "associativity law" should {
+      s"hold for $description" in {
+        m.flatMap(f).flatMap(g) === m.flatMap(x => f(x).flatMap(g))
+      }
     }
+
   }
 }
